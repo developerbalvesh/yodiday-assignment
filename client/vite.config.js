41c1edcaa98c7815dee0192fc01732bc982960cf
developerbalvesh <developer.balvesh@gmail.com>
2024-10-29 22:30:58 +0530

    temporary

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,14 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
  plugins: [react()],
- server: {
-   proxy: {
-     '/api': {
-       target: 'https://yodiday-assignment.onrender.com',
-      // target:'http://localhost:8080',
-       changeOrigin: true,
-       rewrite: (path) => path.replace(/^\/api/, '/api'),
-     },
-   },
- },
+//  server: {
+//    proxy: {
+//      '/api': {
+//        target: 'http://localhost:8080',
+//        changeOrigin: true,
+//        rewrite: (path) => path.replace(/^\/api/, '/api'),
+//      },
+//    },
+//  },
 })
