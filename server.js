c25fc76f3dcfd61a1dd1ddf0ac30c14a4befb6b6
developerbalvesh <developer.balvesh@gmail.com>
2024-10-29 22:41:32 +0530

    dist added

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require("express");
 const itemRoute = require('./routes/itemRoute.js')
 const path = require('path');
 
+// Get the __filename and __dirname in ES module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+
 // rest object
 const app = express();
 
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, './client/dist'))); //middleware for build
+
 
 app.use('/api/v1/items',itemRoute)
 
 // rest api
-app.get('/',(req,res)=>{
-   res.send({
-       message:'welcome to Yodiday'
-   })
-})
+// rest api
+app.use('*',function(req, res){
+    res.sendFile(path.join(__dirname, './client/dist/index.html'))
+  })
 
 
 // PORT
