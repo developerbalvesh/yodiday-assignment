95bc2e769fe504ea1ec236735eff1f3cbb59869b
developerbalvesh <developer.balvesh@gmail.com>
2024-10-30 10:08:06 +0530

    test 2

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const itemRoute = require("./routes/itemRoute.js");
 const path = require("path");
 const cors = require("cors");
+const dotenv = require("dotenv")
 
 // rest object
 const app = express();
+dotenv.config();
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
@@ -20,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 // PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("App is running!");
